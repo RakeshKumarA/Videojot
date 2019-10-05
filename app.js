fc79e685c734d505842e01687e8c1e8682162694
RakeshKumarA <rkakumar85@gmail.com>
2019-10-05 16:09:09 +0200

    Completed form fill

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
-// Map global promise
-//mongoose.Promise = global.Promise;
 // Connect to Mongoose
 mongoose
   .connect('mongodb://localhost/vidjot-dev', {
@@ -32,6 +33,29 @@ app.set('view engine', 'handlebars');
 //bodyparser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Middleware for method-override
+app.use(methodOverride('_method'));
+
+//Express session middleware, secret: secret, resave:true and remove cookie
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+//middleware for connect-flash
+app.use(flash());
+
+//Set Global variables for flash
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error-msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Index Route
 app.get('/', (req, res) => {
@@ -62,6 +86,17 @@ app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
 });
 
+//Edit Idea form
+app.get('/ideas/edit/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    res.render('ideas/edit', {
+      idea: idea
+    });
+  });
+});
+
 //Process form
 app.post('/ideas', (req, res) => {
   let errors = [];
@@ -85,9 +120,34 @@ app.post('/ideas', (req, res) => {
     };
 
     new Idea(newUser).save().then(idea => {
+      req.flash('success_msg', 'Video idea added');
       res.redirect('/ideas');
     });
   }
+});
+
+//Edit form process
+app.put('/ideas/:id', (req, res) => {
+  Idea.findOne({
+    _id: req.params.id
+  }).then(idea => {
+    (idea.title = req.body.title), (idea.details = req.body.details);
+
+    idea.save().then(idea => {
+      req.flash('success_msg', 'Video idea successfully edited');
+      res.redirect('/ideas');
+    });
+  });
+});
+
+//Delete idea
+app.delete('/ideas/:id', (req, res) => {
+  Idea.remove({
+    _id: req.params.id
+  }).then(() => {
+    req.flash('success_msg', 'Video idea removed');
+    res.redirect('/ideas');
+  });
 });
 
 const port = 5000;
